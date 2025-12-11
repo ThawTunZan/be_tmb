d491b896ab2dfa9cd87a5ec72d0bba8b7937839d
ThawTunZan <110762796+ThawTunZan@users.noreply.github.com>
2025-12-12 04:09:45 +0800

    add api endpoints that calls to tarkov.dev and get all the items and a particular item

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,80 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*', // for local dev; in prod set to your origin
+	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// Handle CORS preflight requests
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
+		}
+
+		const url = new URL(request.url);
+		const path = url.pathname;
+
+		if (path == '/items') {
+			const query = `
+		{
+			items {
+				id
+				name
+				shortName
+			}
+		}
+		`;
+			const upstream = await fetch('https://api.tarkov.dev/graphql', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ query }),
+			});
+			if (!upstream.ok) {
+				return new Response(`Upstream error: ${upstream.status}`, { status: 500 });
+			}
+			const data = await upstream.json();
+
+			return new Response(JSON.stringify(data, null, 2), {
+				headers: {
+					'Content-Type': 'application/json',
+					...corsHeaders,
+				},
+			});
+		} else if (path.startsWith('/items/')) {
+			const itemName = path.split('/')[2];
+			const query = `
+		{
+			items(filter: {name: "${itemName}"}) {
+				id
+				name
+				shortName
+			}
+		}
+		`;
+			const upstream = await fetch('https://api.tarkov.dev/graphql', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ query }),
+			});
+			if (!upstream.ok) {
+				return new Response(`Upstream error: ${upstream.status}`, { status: 500 });
+			}
+			const data = await upstream.json();
+
+			return new Response(JSON.stringify(data, null, 2), {
+				headers: {
+					'Content-Type': 'application/json',
+					...corsHeaders,
+				},
+			});
+		}
+		return new Response('Not found', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
